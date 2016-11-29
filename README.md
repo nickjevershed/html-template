54d5d1b91eaa70d6c471a0ecc900cc5fd6a98da1
nickjevershed <nick.evershed@gmail.com>
2016-11-29 15:10:03 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hmtl template for Guardian Australia
+
+This is a basic html template for graphics at Guardian Australia
---
+++ js/main.js
@@ -0,0 +1,11 @@
+//Guardian-specific responsive iframe function
+
+iframeMessenger.enableAutoResize();
+
+function init(data) {
+
+
+} // end init
+
+init();
+
